fa4493d4056863a46f360412236c121627c06664
Luiza Pozzobon <lupzbn@klone-login03.hyak.local>
2025-02-09 11:00:05 -0800

    chore: 7B model works on this version with 2 L40s
    Around 32min to run each task with 7B gemma with `bash run_fewshot.sh`
    
    Around 21min for the 2B model

--- run_fewshot.sh
+++ run_fewshot.sh
@@ -11,5 +11,7 @@ datasets=(
 # Loop through each main dataset and run the script
 for dataset in "${datasets[@]}"
 do
-    python stableprompt_tc.py --dataset $dataset --epoch 100 --update_term 5
+    python stableprompt_tc.py --dataset $dataset --epoch 100 --update_term 5 --cache_dir ./cache
 done
+
+# --agent_model google/gemma-1.1-2b-it --target_model google/gemma-1.1-2b-it
